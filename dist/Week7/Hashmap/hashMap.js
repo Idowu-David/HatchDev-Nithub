7238001f7656b52662d7956902604c897913ada7
David Idowu <idowudavidodun@gmail.com>
2025-07-03 13:59:02 +0100

    Todo: print collision

--- dist/Week7/Hashmap/hashMap.js
+++ dist/Week7/Hashmap/hashMap.js
@@ -22,10 +22,22 @@ class HashMap {
     set(key, value) {
         const node = new hashTable_1.HashNode(key, value);
         const index = (0, hashFunction_1.hashDjb2)(key) % SIZE;
+        let update = false;
         let head = this.table.array[index];
         if (head) {
             // position is not empty
-            node.next = this.table.array[index];
+            while (head) {
+                // duplicate key
+                if (head.key === key) {
+                    this.update(key, value);
+                    update = true;
+                    break;
+                }
+                head = head.next;
+            }
+            if (!update) {
+                node.next = this.table.array[index];
+            }
         }
         this.table.array[index] = node;
     }
@@ -50,21 +62,53 @@ class HashMap {
             return null;
         }
     }
+    /**
+     * Updates the value of a Key in the Hashmap
+     * @param key
+     * @param value
+     */
+    update(key, value) {
+        const index = (0, hashFunction_1.hashDjb2)(key) % SIZE;
+        let head = this.table.array[index];
+        if (head) {
+            while (head) {
+                if (head.key === key) {
+                    head.value = value;
+                }
+                head = head.next;
+            }
+        }
+        else {
+            console.log(`"${key}" doesn't exist`);
+            return;
+        }
+    }
     print(map) {
         if (map) {
             let hashStr = "{";
-            let printed = 0;
+            let printed = false;
+            let collision = false;
             for (let i = 0; i < SIZE; i++) {
                 let node = this.table.array[i];
                 while (node) {
                     if (printed)
                         hashStr += ", ";
-                    hashStr += node.key;
-                    hashStr += ": ";
-                    hashStr += node.value;
-                    printed = 1;
+                    hashStr += `[${node.key}: ${node.value}]`;
+                    if (node.next) {
+                        hashStr += "->";
+                        collision = true;
+                    }
+                    printed = true;
                     node = node.next;
                 }
+                // while (node) {
+                //   if (printed) hashStr += ", ";
+                //   hashStr += node.key;
+                //   hashStr += ": ";
+                //   hashStr += node.value;
+                //   printed = true;
+                //   node = node.next;
+                // }
             }
             hashStr += "}";
             console.log(hashStr);
@@ -73,11 +117,15 @@ class HashMap {
 }
 const map = new HashMap();
 map.set("David", 1);
-map.set("Heyy", 2);
+map.set("Subgenera", 2);
 map.set("Hello", 3);
-map.set("Hello", 3);
-map.set("Hello", 3);
-map.set("Hello", 3);
+map.set("dram ", 5);
+map.set("vivency", 100);
+// map.set("ALX", 10);
+map.set("urites", 89);
+map.set("redescribed", 72);
+map.update("hi", 6);
+// console.log(map.get("Hello"));
 // map.set("depravement", 4);
 // map.set("subgenera", 5);
 map.print(map);
