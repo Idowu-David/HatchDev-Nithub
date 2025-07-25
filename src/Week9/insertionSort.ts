0d4d3d555d9e02ec4c6265dc541530807e0c88bc
David Idowu <idowudavidodun@gmail.com>
2025-07-25 13:59:06 +0100

    bubble and insertion sort pseudocode

--- src/Week9/insertionSort.ts
+++ src/Week9/insertionSort.ts
@@ -10,3 +10,19 @@ function insertionSort(arr: number[]): number[] {
   }
   return arr;
 }
+
+let a = [3, 0, 9, 5, -2];
+
+function insertionS(arr: number[]) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
